c5949a88056a5fb1195a294aeea26371e8df3156
watermelonjuice14 <f20240768@pilani.bits-pilani.ac.in>
2026-08-29 09:15:35 +0000

    lab01 task3

--- labs/lab01/task3/FA_Gate.v
+++ labs/lab01/task3/FA_Gate.v
@@ -18,19 +18,19 @@
 // ripple_adder.v and tb.v; nothing else needs to change.
 
 module FA_Gate(
-  input  a,
-  input  b,
-  input  cin,
+  input a,
+  input b,
+  input cin,
   output sum,
   output cout
 );
+
   wire ps, pc1, pc2;
-  
-  xor #(2,3) (ps, a, b);
-  and #(2,3) (pc1, a, b);
-  xor #(2,3) (sum, cin, ps);
-  and #(2,3) (pc2, cin, ps);
-  or  #(2,3) (cout, pc1, pc2);
-  
+
+  xor #(2) (ps, a, b);
+  and #(2) (pc1, a, b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
