06e56b5c186bf065404e101ae70d10b28af9a45d
watermelonjuice14 <f20240768@pilani.bits-pilani.ac.in>
2026-08-29 09:35:49 +0000

    lab01 task4

--- labs/lab01/task4/dut.v
+++ labs/lab01/task4/dut.v
@@ -1,6 +1,6 @@
 // dut.v
 // Task 4 wrapper
-// Option 2: cla64_flat is active
+// Option 1: rca64 is currently active.
 
 module dut(
   input  [63:0] a,
@@ -10,22 +10,11 @@ module dut(
   output        cout
 );
 
-  // ----------------------------------------------------
-  // Option 1: 64-bit ripple-carry adder
-  // ----------------------------------------------------
-  // rca64 U_IMPL (
-  //   .a(a),
-  //   .b(b),
-  //   .cin(cin),
-  //   .sum(sum),
-  //   .cout(cout)
-  // );
-
-  // ----------------------------------------------------
-  // Option 2: flat 64-bit carry-lookahead adder
-  // ACTIVE FOR TASK 4(a)
-  // ----------------------------------------------------
-  cla64_flat U_IMPL (
+  // ---------------------------------------------
+  // Option 1: 64-bit Ripple Carry Adder
+  // ACTIVE
+  // ---------------------------------------------
+  rca64 U_IMPL (
     .a(a),
     .b(b),
     .cin(cin),
@@ -33,9 +22,20 @@ module dut(
     .cout(cout)
   );
 
-  // ----------------------------------------------------
-  // Option 3: blocked 64-bit carry-lookahead adder
-  // ----------------------------------------------------
+  // ---------------------------------------------
+  // Option 2: Flat 64-bit CLA
+  // ---------------------------------------------
+  // cla64_flat U_IMPL (
+  //   .a(a),
+  //   .b(b),
+  //   .cin(cin),
+  //   .sum(sum),
+  //   .cout(cout)
+  // );
+
+  // ---------------------------------------------
+  // Option 3: Blocked 64-bit CLA
+  // ---------------------------------------------
   // cla64_blocked U_IMPL (
   //   .a(a),
   //   .b(b),
